8590db0dd38226d9c9776a0294ad8aeea909ab9b
mirian228 <pariksini@mail.ru>
2023-04-05 21:34:59 +0400

    Dependency Injection using Setter method

--- src/main/java/com/newproj/spring/music/MusicPlayer.java
+++ src/main/java/com/newproj/spring/music/MusicPlayer.java
@@ -2,14 +2,20 @@ package com.newproj.spring.music;
 
 public class MusicPlayer {
 	private IMusic music;
-	
-	
+
 	// IoC ( Inversion of control)
 	public MusicPlayer(IMusic music) {
 		this.music = music;
 	}
-	
-	
+
+	public MusicPlayer() {
+
+	}
+
+	public void setMusic(IMusic music) {
+		this.music = music;
+	}
+
 	public void playMusic() {
 		System.out.println("Playing: " + music.getSong());
 	}
