c42903f3553f055b9db3af11c625b110611845fc
mirian228 <pariksini@mail.ru>
2023-04-07 20:06:12 +0400

    Implemented Init-method, destroy-method, Factory Method

--- src/main/java/com/newproj/spring/TestSpring.java
+++ src/main/java/com/newproj/spring/TestSpring.java
@@ -3,29 +3,35 @@ package com.newproj.spring;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.newproj.spring.animal.AnimalSounds;
+import com.newproj.spring.music.ClassicalMusic;
 import com.newproj.spring.music.MusicPlayer;
 
 public class TestSpring {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		//IMusic music = context.getBean("musicBean", IMusic.class);
-		//MusicPlayer musicPlayer = new MusicPlayer(music);
-		
-		
+		// IMusic music = context.getBean("musicBean", IMusic.class);
+		// MusicPlayer musicPlayer = new MusicPlayer(music);
+
 		MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+		MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 		musicPlayer.playMusic();
 		System.out.println(musicPlayer.getName());
 		System.out.println(musicPlayer.getVolume());
-		
-	
-		
-	
+
+		System.out.println("Scope = prototype: Check");
+		boolean comparison = musicPlayer == musicPlayer2;
+		System.out.println(comparison);
+		System.out.println(musicPlayer);
+		System.out.println(musicPlayer2);
+
 		AnimalSounds animalSounds = context.getBean("animalSounds", AnimalSounds.class);
 		animalSounds.getAnimalSound();
+
+		ClassicalMusic classicalMusic = context.getBean("musicBeanClassical", ClassicalMusic.class);
+		System.out.println(classicalMusic.getSong());
 		
 		
 		context.close();
 
-		
 	}
 }
--- src/main/java/com/newproj/spring/music/ClassicalMusic.java
+++ src/main/java/com/newproj/spring/music/ClassicalMusic.java
@@ -1,6 +1,27 @@
 package com.newproj.spring.music;
 
 public class ClassicalMusic implements IMusic {
+	
+	//Factory pattern uses private constructor
+	private ClassicalMusic() {
+		
+	}
+	
+	//Static initializer (Part of factory pattern) which returns instance of classical music.
+	public static ClassicalMusic getClassicalMusic() {
+		return new ClassicalMusic();
+	}
+	
+	
+	public void doMyInit() {
+		System.out.println("Doing my initialization");
+	}
+	
+	public void doMyDestroy() {
+		System.out.println("Doing my destruction");
+	}
+	
+	
 	@Override
 	public String getSong() {
 		return "Hungarian Rhapsody";
